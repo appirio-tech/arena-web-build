d3b077d8a7d25b42bd8b8f20a63b6cd1972e153c
Mauricio Desiderio <mdesiderio@appirio.com>
2014-05-20 08:05:02 -0500

    updating version due to cdn cache

--- config/dev-local.sh
+++ config/dev-local.sh
@@ -33,3 +33,15 @@ export AWS_ACCESS_KEY=
 export AWS_BUCKET=
 export AWS_FOLDER='arena/md/web-v<%= pkg.version %>/'
 
+NODE_ENV=UAT
+AUTH0_DOMAIN=topcoder.auth0.com
+AUTH0_CLIENT_ID=6ZwZEUo2ZK4c50aLPpgupeg5v2Ffxp9P
+AUTH0_CONNECTION=LDAP
+CALLBACK_URL=https://www.topcoder.com/reg2/callback.action
+WEB_SOCKET_URL=https://arenaws.topcoder.com:443
+SSO_KEY=tcsso
+STATIC_FILE_HOST='http://localhost:3000'
+AWS_ACCESS_KEY_ID=AKIAJWJA57TSTT34KAVA
+AWS_ACCESS_KEY=j6U/nRPjPXGzarZRgyJjrx3Fk0ZNkopzvWlnxN9n
+AWS_BUCKET=tc-content
+AWS_FOLDER='arena/prod/web-v<%= pkg.version %>/'
